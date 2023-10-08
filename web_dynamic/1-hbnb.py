b249183deec5d0f21c4f3ea696ff6862269e1445
Herman100 <hermankwamebour30@gmail.com>
2023-10-08 21:32:39 +0000

    select  some Amenities to be comfortable!

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 from os import environ
 import uuid
 from flask import Flask, render_template
@@ -15,23 +19,23 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    states = storage.all('State').values()
+    states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-    amenities = storage.all('Amenity').values()
+    amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
 
-    places = storage.all('Place').values()
+    places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
