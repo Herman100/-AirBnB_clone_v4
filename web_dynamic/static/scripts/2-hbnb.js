f80f1bb104ce942eff16a42313600fae42122a25
Herman100 <hermankwamebour30@gmail.com>
2023-10-08 23:14:54 +0000

    api status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  let amenityDict = {};
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      amenityDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityDict[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
